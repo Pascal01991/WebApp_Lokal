b075a1f96d405732ebf125ab2bf796217f020d3f
Pascal <pascalino.schmidiger@gmail.com>
2024-11-30 00:18:29 +0100

    V5.28.3_Loign_Backend

---
+++ frontend/config.js
@@ -0,0 +1,3 @@
+
+//Globale Backend-URL
+export const BACKEND_URL = "https://sapps.ch/api";
--- frontend/login.js
+++ frontend/login.js
@@ -1,5 +1,5 @@
 // Importiere die Backend-URL (muss am Anfang stehen)
-import { BACKEND_URL } from './server.js';
+import { BACKEND_URL } from './config.js';
 
 // Füge den Event-Listener für das Formular hinzu
 document.getElementById('loginForm').addEventListener('submit', async function(e) {
