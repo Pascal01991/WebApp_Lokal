48b58c03ab38f160b8b5ccdcb81fb18df51fd7f8
Pascal <pascalino.schmidiger@gmail.com>
2024-12-09 20:06:38 +0100

    V6.0.0.2.7_Test_Parallel_Lokal/Produktiiv

--- frontend/config.js
+++ frontend/config.js
@@ -1,13 +1,14 @@
 
-
+/*
 // Globale Backend-URL basierend auf der Umgebung
 export const BACKEND_URL =
     window.location.hostname === 'localhost' || window.location.hostname === '127.0.0.1'
         ? 'http://localhost:5000/api' // Lokales Backend für lokale Tests
         : 'https://sapps.ch/api'; // Produktions-Backend für Live-System
 
-
+/*
 
 /* //Lokalerbetrieb funktioniert so:
 export const BACKEND_URL = "http://localhost:5000/api";
 */
+export const BACKEND_URL = "https://sapps.ch/api";
