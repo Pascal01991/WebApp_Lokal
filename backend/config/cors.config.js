36cdbf26709300aef22d3853b913da1af4758609
Pascal <pascalino.schmidiger@gmail.com>
2025-01-25 11:04:58 +0100

    Alpha_V_0.0.0.2_.dotENV_update

--- backend/config/cors.config.js
+++ backend/config/cors.config.js
@@ -6,9 +6,20 @@ const allowedOrigins = [
   //'http://127.0.0.1:8080',  // Lokale Entwicklung (Alternative)
     'http://localhost:5000',  // Lokale Entwicklung
 
-    'https://www.sapps.ch',    // Deine Produktionsdomain  https://www.
-    'https://sapps.ch'          // Deine Produktionsdomain  https://
-];
+     // Hauptdomain der Instanz (aus .env)
+    process.env.DASHBOARD_APP_DOMAIN,
+    `https://${process.env.DASHBOARD_APP_DOMAIN}`,
+    `https://www.${process.env.DASHBOARD_APP_DOMAIN}`
+
+    // Zusätzliche Domains (Beispiel)
+  //...(process.env.EXTRA_CORS_DOMAINS ? 
+////  : [])
+    ].filter(Boolean);
+
+//OLD wird ab zweiter Instanz nicht mehr benötigt:
+  //  'https://www.sapps.ch',    // Deine Produktionsdomain  https://www.
+ //   'https://sapps.ch'          // Deine Produktionsdomain  https://
+//];
 
 
 
